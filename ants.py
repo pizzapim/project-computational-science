e98706f8b266c586fc629b96d1ba7f80794749bf
Pim Kunis <pim@kunis.nl>
2021-01-09 18:03:54 +0100

    Add unfinished ant sensing algorithm

--- ants.py
+++ ants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from enum import IntEnum
-from random import random, randint
+from random import random, randint, choice
 
 from cellular_automaton import CellularAutomaton, VonNeumannNeighborhood, CAWindow, EdgeRule
 
@@ -41,9 +41,43 @@ class AntsCA(CellularAutomaton):
 
     
     def evolve_rule(self, last_cell_state, neighbors_last_states):
+        (cell, _) = last_cell_state
+        
+        # Turn the ants
+        if cell != Cell.EMPTY and cell != Cell.BORDER:
+            self.sense(last_cell_state, neighbors_last_states)
+
         return last_cell_state
 
     
+    # Turn the ants in the CA; see page 453 of the book
+    def sense(self, last_cell_state, neighbors_last_states):
+        max_pher = float('-inf')
+        max_cells = []
+
+        # TODO: not sure how to implement rule 2 yet
+        for (i, (cell, cell_pher)) in enumerate(neighbors_last_states):
+            # If the cell already contains an ant, set pheromones to -2
+            if cell != Cell.EMPTY and cell != Cell.BORDER:
+                pher = -2.
+            else:
+                pher = cell_pher
+            
+            # Find new max pheromones cell or add to list
+            if pher > max_pher:
+                max_pher = pher
+                max_cells = [i]
+            elif pher == max_pher:
+                max_cells.append(i)
+        
+        if max_pher < 0.:
+            return Cell.STAY
+        
+        rand_i = choice(max_cells)
+        # TODO: we will need to turn to the direction of the cell,
+        # but we have no information about coordinates from the library...
+        
+
     @staticmethod
     def state_to_color(state):
         (cell, _) = state
