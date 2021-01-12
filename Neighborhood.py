385150bec35deab0a05c9bf01e948810e2de8cff
Pim Kunis <pim@kunis.nl>
2021-01-12 13:56:31 +0100

    Improve complexity of neighborhood calculation

--- Neighborhood.py
+++ Neighborhood.py
@@ -1,20 +1,13 @@
 class VonNeumannNeighborhood():
 
     def __init__(self, r=1):
-        self.coords = []
-
-        # Middle row
-        for x in range(-r, r+1):
-            self.coords.append((x, 0))
+        self.coords = [(x, 0) for x in range(-r, r+1) if r != 0]
 
         # Top and bottom triangle
         for i in range(r, 0, -1):
             for x in range(-(r-i), r-i+1):
                 self.coords.append((x, i))
                 self.coords.append((x, -i))
-
-        # Center removed
-        self.coords.remove((0,0))
 
 
     def for_coords(self, x, y, N):
