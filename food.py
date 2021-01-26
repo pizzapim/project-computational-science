7a73c400633a3a635ecc9e8dc7e36dea37fa24bc
Roberto Volpe Garcia <robertovolpegarcia@gmail.com>
2021-01-26 01:33:21 +0100

    Added 3d graph experiment and code

--- food.py
+++ food.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from AntsCA import AntsCA, Cell
 
+from mpl_toolkits import mplot3d
 
 # Pairs of number of food sources & amount of food per source.
 # All add up to 12 food.
@@ -26,6 +27,37 @@ inputs = [
     (180, 1)
 ]
 
+def experiment3D(filename, n):
+    src = np.arange(10, 110, 10)
+    amt = np.arange(10, 110, 10)
+    srcv, amtv = np.meshgrid(src, amt)
+    meanIts = np.copy(srcv)
+    count = 1
+
+    print("Points to calc: " + str(len(src) * len(amt)))
+
+    for i in range(len(src)):
+        for j in range(len(amt)):
+            print()
+            print("Point nr: " + str(count))
+            it_it = []
+            for time in range(n):
+                print("n: " + str(time + 1))
+                ca = AntsCA(food_sources=src[i], food_amount=amt[j])
+                (nestx, nesty) = ca.NEST_COORD
+
+                while True:
+                    ca.evolve()
+                    food = ca.grid[nesty][nestx][2]
+                    if food >= src[i] * amt[j]:
+                        break
+                it_it.append(len(ca.counter))
+            count += 1
+
+            meanIts[j][i] = int(np.mean(it_it))
+
+    pickle.dump((srcv, amtv, meanIts), open(filename, "wb"))
+
 
 def experiment(filename, n):
     results = {}
@@ -38,16 +70,29 @@ def experiment(filename, n):
             while True:
                 ca.evolve()
                 food = ca.grid[nesty][nestx][2]
-                if food == sources * amount:
+                if food >= sources * amount:
                     break
             food = [i[0] for i in ca.counter]
             on_pher = [i[1] for i in ca.counter]
             results[(sources, amount)].append((food, on_pher))
             count += 1
             print(count)
-    
+
     pickle.dump(results, open(filename, "wb"))
-        
+
+def graph3d(filename):
+    X, Y, Z = pickle.load(open(filename, "rb"))
+
+    print(X)
+    print(Y)
+    print(Z)
+
+    plt.figure(figsize=(8,5))
+    ax = plt.axes(projection ='3d')
+
+    ax.plot_surface(X, Y, Z, cmap ='viridis', edgecolor ='green')
+    plt.show()
+
 
 def graph(filename):
     # Structure of results is a dictionary with keys (number of sources, amount per source)
